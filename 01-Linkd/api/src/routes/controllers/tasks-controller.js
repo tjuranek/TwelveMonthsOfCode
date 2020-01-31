1a9cd8542bb74ebc06e503418c872b66e558e4a1
Thomas Juranek <thomas@juranek.com>
2020-01-30 18:34:23 -0600

    clean up

--- 01-Linkd/api/src/routes/controllers/tasks-controller.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllTasks } from '../middlewares/tasks-middleware.js';
-
-const taskRouter = express.Router();
-
-taskRouter.get('/all', getAllTasks);
-
-export { taskRouter };
--- 01-Linkd/api/src/routes/middlewares/tasks-middleware.js
+++
@@ -1,8 +0,0 @@
-import { connection } from '../../db/index.js';
-
-const getAllTasks = async (req, res) => {
-	const tasks = await connection.query('select * from Links');
-	res.send(tasks);
-};
-
-export { getAllTasks };
