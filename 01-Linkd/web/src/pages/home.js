4f53275b604c1cab0901c90bb62fd54bb7cfad41
Thomas Juranek <thomas@juranek.com>
2020-03-12 15:13:56 -0500

    working cenetered home page grid

--- 01-Linkd/web/src/pages/home.js
+++ 01-Linkd/web/src/pages/home.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 
 import { Logo } from '../components/logo';
 import { SingleInputForm } from '../components/single-input-form';
 import { Footer } from '../components/footer';
 
+const useStyles = createUseStyles({
+	grid: {
+		display: 'grid',
+		gridTemplateRows: '1fr 2fr 1fr',
+		gridTemplateColumns: '1fr 2fr 1fr',
+		gridTemplateAreas: `
+			'. . .'
+			'. content .'
+			'. . .'
+		`,
+		height: '100vh',
+		width: '100vw'
+	},
+	content: {
+		backgroundColor: 'red',
+		gridArea: 'content'
+	}
+});
+
 const Home = () => {
+	const classes = useStyles();
+
 	return (
-		<>
-			<Logo />
-			<SingleInputForm />
-			<Footer />
-		</>
+		<div className={classes.grid}>
+			<div class="content" className={classes.content}>
+				<Logo />
+				<SingleInputForm />
+				<Footer />
+			</div>
+		</div>
 	);
 };
 
