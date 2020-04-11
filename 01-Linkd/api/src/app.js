5b1b7ea48f5f2b50ec53b8f1b8a14050615f8eb2
Thomas Juranek <thomas@juranek.com>
2020-04-11 11:50:58 -0500

    fix json parsing

--- 01-Linkd/api/src/app.js
+++ 01-Linkd/api/src/app.js
@@ -7,11 +7,10 @@ import { router } from './routes/router.js';
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(bodyParser.text({ type: 'text/html' }));
 
 app.use(cors());
-
-app.use(express.json({ type: 'application/*+json' }));
 
 app.use('/', router);
 
