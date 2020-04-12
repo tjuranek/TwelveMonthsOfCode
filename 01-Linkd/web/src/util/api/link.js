710534bae98af40d156d4b540e8ba18208955aed
Thomas Juranek <thomas@juranek.com>
2020-04-11 21:56:10 -0500

    add create-react-app .env.development

--- 01-Linkd/web/src/util/api/link.js
+++ 01-Linkd/web/src/util/api/link.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
+const siteUrl = process.env.REACT_APP_SITE_URL;
+
 const getLink = async (key) => {
-	const response = await axios.get(`http://localhost:3001/link/get/${key}`);
+	const response = await axios.get(`${siteUrl}/link/get/${key}`);
 
 	return response.status === 200 ? response.data : '';
 };
 
 const submitLink = async (link) => {
-	const response = await axios.post('http://localhost:3001/link/create', {
+	const response = await axios.post(`${siteUrl}/link/create`, {
 		link: link,
 	});
 
